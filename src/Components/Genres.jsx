e663506f26b6c74d4cd7d5caea9c78b43d5df2f4
Jamie Ohax <jimohax@gmail.com>
2023-03-27 14:09:25 +0100

    genre select

--- src/Components/Genres.jsx
+++ src/Components/Genres.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { genres } from "../Services/fakeGenreService";
 
-const Genres = ({ onItemSelect }) => {
+const Genres = ({ onItemSelect, selectedItem }) => {
 	return (
 		<ul className="list-group" style={{ cursor: "pointer" }}>
 			<li className="list-group-item active">All Genres</li>
@@ -9,11 +9,12 @@ const Genres = ({ onItemSelect }) => {
 				return (
 					<li
 						className={
-							"list-group-item "
+							g.name == selectedItem? "list-group-item active": "list-group-item"
 						}
 						style={{ cursor: "pointer" }}
 						key={index}
 						onClick={() => onItemSelect(g)}
+
 					>
 						{g.name}
 					</li>
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -8,8 +8,9 @@ import Genres from "./Genres";
 const Movies = () => {
 	// let mov =  getMovies();
 	const [movies, setMovies] = useState(getMovies());
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postPerPage, setPostPerPage] = useState(3);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [postPerPage, setPostPerPage] = useState(3);
+	const [genreSelect, setGenreSelect] = useState('');
 
 	const newMovies = paginate(movies, currentPage, postPerPage);
 
@@ -17,83 +18,81 @@ const Movies = () => {
 		const newMovies = movies.filter((m) => m._id !== movie._id);
 
 		setMovies(newMovies);
-		   console.log(newMovies.length);
+		console.log(newMovies.length);
 		// return movies;
 	};
 
-    // const lastPostIndex = currentPage * postPerPage ;
-    // const firstPostIndex = lastPostIndex - postPerPage;
-    // const currentPosts = movies.splice(firstPostIndex, lastPostIndex);
+	// const lastPostIndex = currentPage * postPerPage ;
+	// const firstPostIndex = lastPostIndex - postPerPage;
+	// const currentPosts = movies.splice(firstPostIndex, lastPostIndex);
 
 	// console.log(firstPostIndex, lastPostIndex);
 	// console.log(currentPosts);
 
-const handlePageChange = (page)=>{
-	setCurrentPage(page, currentPage, postPerPage);
-}
-    const handleSelect = (g)=>{
-		console.log(g.name);
-	}
-// console.log(movies);
+	const handlePageChange = (page) => {
+		setCurrentPage(page, currentPage, postPerPage);
+	};
+	const handleSelect = (g) => {
+		setGenreSelect(g.name);
+		// console.log(g);
+	};
+	// console.log(movies);
 
 	return (
 		<div className="row">
 			<div className="col-2">
-				<Genres  onItemSelect={handleSelect}/>
+				<Genres onItemSelect={handleSelect} selectedItem={genreSelect} />
 			</div>
 			<div className="col">
-			<h4>
-				{newMovies.length < 1
-					? "There are no movies in the database"
-					: "Showing " + newMovies.length + " movies in the database"}
-			</h4>
-			<table className="table">
-				<thead>
-					<tr>
-						<th scope="col">Title</th>
-						<th scope="col">Genre</th>
-						<th scope="col">Stock</th>
-						<th scope="col">Rate</th>
-						<th/>
-						<th/>
-					</tr>
-				</thead>
-				<tbody>
-					{newMovies.map((m, index) => {
-						return (
-							<tr key={m._id}>
-								<td>{m.title}</td>
-								<td>{m.genre.name}</td>
-								<td>{m.numberInStock}</td>
-								<td>{m.dailyRentalRate}</td>
-								<td>
-									<Like/>
-								</td>
-								<td>
-									<button
-										onClick={() => handleDelete(m)}
-										className="btn btn-danger btn-sm"
-									>
-										Delete
-									</button>
-								</td>
-							</tr>
-                                
-						);
-					})}
-				</tbody>
-			</table>
-			<Pagination  
-    totalPosts={movies.length}
-    postPerPage={postPerPage}
-	 onPageChange={handlePageChange}
-	 currentPage={currentPage}
-    />
+				<h4>
+					{newMovies.length < 1
+						? "There are no movies in the database"
+						: "Showing " +
+						  newMovies.length +
+						  " movies in the database"}
+				</h4>
+				<table className="table">
+					<thead>
+						<tr>
+							<th scope="col">Title</th>
+							<th scope="col">Genre</th>
+							<th scope="col">Stock</th>
+							<th scope="col">Rate</th>
+							<th />
+							<th />
+						</tr>
+					</thead>
+					<tbody>
+						{newMovies.map((m, index) => {
+							return (
+								<tr key={m._id}>
+									<td>{m.title}</td>
+									<td>{m.genre.name}</td>
+									<td>{m.numberInStock}</td>
+									<td>{m.dailyRentalRate}</td>
+									<td>
+										<Like />
+									</td>
+									<td>
+										<button
+											onClick={() => handleDelete(m)}
+											className="btn btn-danger btn-sm"
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+				<Pagination
+					totalPosts={movies.length}
+					postPerPage={postPerPage}
+					onPageChange={handlePageChange}
+					currentPage={currentPage}
+				/>
 			</div>
-			
-            
-    
-    
 		</div>
 	);
 };
